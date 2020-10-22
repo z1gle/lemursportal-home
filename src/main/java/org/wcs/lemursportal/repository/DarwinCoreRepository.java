694aa7db3731900322bc122f51241e786a0ecab3
ROROHERO1 <vmrsojandrimalala@gmail.com>
2020-10-22 15:31:57 +0300

    Modification requête

--- src/main/java/org/wcs/lemursportal/repository/DarwinCoreRepository.java
+++ src/main/java/org/wcs/lemursportal/repository/DarwinCoreRepository.java
@@ -29,8 +29,8 @@ public interface DarwinCoreRepository extends
      @Query(value = "Select distinct(d.scientificname),\n"
             + "(select count(ts.iddwc) from vue_validation_darwin_core ts where ts.scientificname=d.scientificname ) as tous,\n"
             + "(select count(r.iddwc) from vue_validation_darwin_core r where r.validationexpert=1 and r.scientificname=d.scientificname )as reviewed,\n"
-            + "(select count(a.iddwc) from vue_validation_darwin_core a where a.validationexpert=0 and a.scientificname=d.scientificname )as attente_validation,\n"
-            + "(select count(q.iddwc) from vue_validation_darwin_core q where q.validationexpert=-1 and q.scientificname=d.scientificname )as questionnable\n"
+            + "(select count(a.iddwc) from vue_validation_darwin_core a where a.validationexpert=-1 and a.scientificname=d.scientificname )as attente_validation,\n"
+            + "(select count(q.iddwc) from vue_validation_darwin_core q where q.validationexpert=0 and q.scientificname=d.scientificname )as questionnable\n"
             + "from darwin_core d\n"
             + "where lower(d.scientificname) like '%' || :keyword || '%' and lower(d.scientificname) not like '-' group by d.scientificname order by d.scientificname limit 2", nativeQuery = true)
     public List<Object[]> findOccurrenncelim(@Param("keyword") String keyword);
@@ -38,8 +38,8 @@ public interface DarwinCoreRepository extends
     @Query(value = "Select distinct(d.scientificname),\n"
             + "(select count(ts.iddwc) from vue_validation_darwin_core ts where ts.scientificname=d.scientificname ) as tous,\n"
             + "(select count(r.iddwc) from vue_validation_darwin_core r where r.validationexpert=1 and r.scientificname=d.scientificname )as reviewed,\n"
-            + "(select count(a.iddwc) from vue_validation_darwin_core a where a.validationexpert=0 and a.scientificname=d.scientificname )as attente_validation,\n"
-            + "(select count(q.iddwc) from vue_validation_darwin_core q where q.validationexpert=-1 and q.scientificname=d.scientificname )as questionnable\n"
+            + "(select count(a.iddwc) from vue_validation_darwin_core a where a.validationexpert=-1 and a.scientificname=d.scientificname )as attente_validation,\n"
+            + "(select count(q.iddwc) from vue_validation_darwin_core q where q.validationexpert=0 and q.scientificname=d.scientificname )as questionnable\n"
             + "from darwin_core d\n"
             + "where lower(d.scientificname) like '%' || :keyword || '%' and lower(d.scientificname) not like '-' group by d.scientificname order by d.scientificname ", nativeQuery = true)
     public List<Object[]> findOccurrennce(@Param("keyword") String keyword);
